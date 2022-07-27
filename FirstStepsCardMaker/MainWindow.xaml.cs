c150f5f84c9b7172ba0dd1a49cfa65ba0c96ead4
jitseV <jitse.verstraete@gmail.com>
2022-07-27 19:17:33 +0200

    show the created image on screen

--- FirstStepsCardMaker/MainWindow.xaml.cs
+++ FirstStepsCardMaker/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Interop;
 
 
 namespace FirstStepsCardMaker
@@ -26,27 +29,34 @@ namespace FirstStepsCardMaker
         {
             InitializeComponent();
 
-            Bitmap image = new Bitmap(400, 400);
-            
-            for(int i = 0; i < image.Width; i++)
+            Bitmap image = new Bitmap(600, 600);
+
+            for (int i = 0; i < image.Width; i++)
             {
                 for (int j = 0; j < image.Height; j++)
                 {
-                    Color c = Color.FromArgb((int)((i / (float)image.Width) * 255) , (int)((j / (float)image.Height) * 255), 255);
-
-
-                    var g =  Graphics.FromImage(image);
-
-                    Font f = new Font(new FontFamily("Arial"), 20);
-                    SolidBrush b = new SolidBrush(Color.Black);
-                    g.DrawString("test", f, b, new PointF(200, 200), StringFormat.GenericDefault);
-
+                    System.Drawing.Color c = System.Drawing.Color.FromArgb((int)((i / (float)image.Width) * 255), (int)((j / (float)image.Height) * 255), 255);
                     image.SetPixel(i, j, c);
+
+
+
                 }
             }
 
+
+            var g = Graphics.FromImage(image);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+            Font f = new Font(new System.Drawing.FontFamily("Arial"), 20, System.Drawing.FontStyle.Regular);
+            SolidBrush b = new SolidBrush(System.Drawing.Color.Black);
+            g.DrawString("test", f, b, new PointF(300, 50), StringFormat.GenericDefault);
+
+
+
+
+            im.Source = Imaging.CreateBitmapSourceFromHBitmap(image.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             image.Save("testImage.png", ImageFormat.Png);
-            
+
         }
     }
 }
